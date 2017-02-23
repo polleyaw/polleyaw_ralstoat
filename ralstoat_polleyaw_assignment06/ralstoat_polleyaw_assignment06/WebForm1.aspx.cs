22bca915e7f027eb3b734be230891fc9d6512d39
polleyaw <polleyaw@mail.uc.edu>
2017-02-22 19:34:02 -0500

    test

--- ralstoat_polleyaw_assignment06/ralstoat_polleyaw_assignment06/WebForm1.aspx.cs
+++ ralstoat_polleyaw_assignment06/ralstoat_polleyaw_assignment06/WebForm1.aspx.cs
@@ -7,11 +7,10 @@ using System.Web.UI.WebControls;
 
 namespace ralstoat_polleyaw_assignment06
 {
-    public partial class Default : System.Web.UI.Page
+    public partial class WebForm1 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //test
             //test
         }
     }
